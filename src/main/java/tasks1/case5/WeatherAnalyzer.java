608dc45f3249e02fa9d18996cdb89e3ca040cf3d
Максим Манзадей <Manzadejmaksim79@gmail.com>
2026-09-23 19:36:52 +0300

    Сообщение сос мыслом о том, что я изменил что-то

--- src/main/java/tasks1/case5/WeatherAnalyzer.java
+++ src/main/java/tasks1/case5/WeatherAnalyzer.java
@@ -1,33 +1,53 @@
-package src.main.java.tasks1.case5;
-
 /**
  * Программа для анализа погоды за неделю.
  * Находит экстремальные температуры, среднюю и количество морозных дней.
  */
 public class WeatherAnalyzer {
-    
+    public static int Min(int[] temps){
+        var min = 1000;
+        for (int t:temps){
+            if (t < min){
+                min = t;
+            }
+        }
+        return min;
+    }
+    public static int Max(int[] temps){
+        var max = -1000;
+        for (int t:temps){
+            if (t > max){
+                max = t;
+            }
+        }
+        return max;
+    }
+    public static int CntFrost(int[] temps){
+        int cnt = 0;
+        for (int t:temps){
+            if (t < 0){
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+
+    public static double Sum(int[] temps){
+        double summa = 0;
+        for (int t:temps){
+            summa += t;
+        }
+        return summa;                           //вынес логику инициализации переменных в отдельные методы
+    }
+
     public static void main(String[] args) {
         int[] temperatures = {-5, 2, -1, 4, -8, 0, 3};
 
-        int min = temperatures[0];
-        int max = temperatures[0];
-        int sum = 0;
-        int frostDays = 0;
+        int min = Min(temperatures);
+        int max = Max(temperatures);
+        double sum = Sum(temperatures);
+        int frostDays = CntFrost(temperatures); //не считались отрицательные значения
 
-        for (int i = 0; i < temperatures.length; i++) {
-            if (temperatures[i] < min) {
-                min = temperatures[i];
-            }
-            if (temperatures[i] > max) {
-                max = temperatures[i];
-            }
-            sum += temperatures[i];
-            if (temperatures[i] < 0) {
-                frostDays++;
-            }
-        }
-
-        int average = sum / temperatures.length; 
+        double average = sum / temperatures.length;      //изменил тип данных для точности
 
         System.out.println("Минимальная температура: " + min);
         System.out.println("Максимальная температура: " + max);
